75436f2676c95bc34d73a4a2684788b1de5c5a31
pakizekus <48360370+pakizekus@users.noreply.github.com>
2020-09-14 15:11:02 +0300

    first-commit

---
+++ ExchangeRates/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ExchangeRates
+{
+    partial class FrmExchangeRates
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.tmr_Refresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(791, 92);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.dataGridView1);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(791, 92);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // tmr_Refresh
+            // 
+            this.tmr_Refresh.Interval = 1000;
+            this.tmr_Refresh.Tick += new System.EventHandler(this.tmr_Refresh_Tick);
+            // 
+            // FrmExchangeRates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(791, 89);
+            this.Controls.Add(this.pnlTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FrmExchangeRates";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Timer tmr_Refresh;
+    }
+}
+
---
+++ ExchangeRates/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Xml;
+using Timer = System.Windows.Forms.Timer;
+
+namespace ExchangeRates
+{
+    public partial class FrmExchangeRates : Form
+    {
+        public FrmExchangeRates()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            RefreshCheck();
+            dataGridView1.DataSource = Source();
+        }
+        public DataTable Source()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                DataRow dr;
+                // DataTable ın satırlarını tanımlıyoruz.
+                dt.Columns.Add(new DataColumn("Adı", typeof(string)));
+                dt.Columns.Add(new DataColumn("Kod", typeof(string)));
+                dt.Columns.Add(new DataColumn("Döviz alış", typeof(string)));
+                dt.Columns.Add(new DataColumn("Döviz satış", typeof(string)));
+                dt.Columns.Add(new DataColumn("Efektif alış", typeof(string)));
+                dt.Columns.Add(new DataColumn("Efektif Satış", typeof(string)));
+
+                XmlTextReader rdr = new XmlTextReader("http://www.tcmb.gov.tr/kurlar/today.xml");
+                // XmlTextReader nesnesini yaratıyoruz ve parametre olarak xml dokümanın urlsini veriyoruz
+                // XmlTextReader urlsi belirtilen xml dokümanlarına hızlı ve forward-only giriş imkanı sağlar.
+                XmlDocument myxml = new XmlDocument();
+                // XmlDocument nesnesini yaratıyoruz.
+                myxml.Load(rdr);
+                // Load metodu ile xml yüklüyoruz
+                XmlNode tarih = myxml.SelectSingleNode("/Tarih_Date/@Tarih");
+                XmlNodeList mylist = myxml.SelectNodes("/Tarih_Date/Currency");
+                XmlNodeList adi = myxml.SelectNodes("/Tarih_Date/Currency/Isim");
+                XmlNodeList kod = myxml.SelectNodes("/Tarih_Date/Currency/@Kod");
+                XmlNodeList doviz_alis = myxml.SelectNodes("/Tarih_Date/Currency/ForexBuying");
+                XmlNodeList doviz_satis = myxml.SelectNodes("/Tarih_Date/Currency/ForexSelling");
+                XmlNodeList efektif_alis = myxml.SelectNodes("/Tarih_Date/Currency/BanknoteBuying");
+                XmlNodeList efektif_satis = myxml.SelectNodes("/Tarih_Date/Currency/BanknoteSelling");
+                for (int i = 0; i < 4; i++)
+                {
+                    dr = dt.NewRow();
+                    dr[0] = adi.Item(i).InnerText.ToString();
+                    dr[1] = kod.Item(i).InnerText.ToString();
+                    // Kod satırları
+                    dr[2] = doviz_alis.Item(i).InnerText.ToString();
+                    // Döviz Alış
+                    dr[3] = doviz_satis.Item(i).InnerText.ToString();
+                    // Döviz  Satış
+                    dr[4] = efektif_alis.Item(i).InnerText.ToString();
+                    // Efektif Alış
+                    dr[5] = efektif_satis.Item(i).InnerText.ToString();
+                    // Efektif Satış.
+                    dt.Rows.Add(dr);
+                    i = i + 2;
+                }
+
+                return dt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        private void tmr_Refresh_Tick(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Source();
+            // MessageBox.Show("Değişti");
+        }
+
+        public void RefreshCheck()
+        {
+            try
+            {
+                Timer tmr = new Timer();
+                tmr.Interval = (60 * 5 * 1000);//5 sec
+                tmr.Tick += new EventHandler(tmr_Refresh_Tick);
+                tmr.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+    }
+}
